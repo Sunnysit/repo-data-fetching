e6ade7f82668f98f95af12faf6ed1f4585f0e801
Sunny Xue <sunny.sit26@gmail.com>
2019-10-08 13:12:35 -0700

    Update single language fetching method

--- script.js
+++ script.js
@@ -2,47 +2,75 @@
  * @Author: Sunny Xue
  * @Date: 2019-10-07 22:07:25
  * @Last Modified by: Sunny Xue
- * @Last Modified time: 2019-10-07 23:46:57
+ * @Last Modified time: 2019-10-08 13:08:25
  */
 
 "use strict;"
+$( document ).ready(function() {
 
 let languages = [
     'Python',
     'Java',
     'Javascript',
-    // 'C#',
-    // 'PHP',
-    // 'C++',
-    // 'R',
-    // 'Objective-C',
-    // 'Swift',
-    // 'Matlab',
-    // 'TypeScript',
-    // 'Kotlin',
-    // 'Ruby',
-    // 'VBA',
-    // 'Go',
-    // 'Rust'
+    'C#',
+    'PHP',
+    'C++',
+    'R',
+    'Objective-C',
+    'Swift',
+    'Matlab',
+    'TypeScript',
+    'Kotlin',
+    'Ruby',
+    'VBA',
+    'Go',
+    'Rust'
 ];
+
+const outputHTML = document.getElementById('output');
+const selectLanguage = document.getElementById('select-language');
+const fetchBtn = document.getElementById('fetch-btn');
+const getJSONBtn = document.getElementById('get-json-btn');
+
+let selectOptions = '';
+languages.map(lang=>{
+    selectOptions+=`<option value="${lang}">${lang}</option>`;
+})
+
+selectLanguage.innerHTML=selectOptions;
+
 
 const timePromise = (languageName,i,searchYear,searchStartMonth,searchStartDay,searchEndMonth,searchEndDay) =>{
     return new Promise(resolve =>  setTimeout(() => {
         
-        let startDate = `${searchYear+i}-0${searchStartMonth}-0${searchStartDay}`;
-        let endDate = `${searchYear+i}-0${searchEndMonth}-${searchEndDay}`;
-        // let url =
-        // `https://api.github.com/search/repositories?q=language%3A${languageName}+created%3A${startDate}..${endDate}`; 
-        // fetch(url)
-        // .then((resp) => resp.json())
-        // .then((data) => 
-        // { 
-        // // console.log(`${startDate}..${endDate}:${data.total_count}`); 
+        let startDate = `${searchYear+i}-${searchStartMonth}-${searchStartDay}`;
+        let endDate = `${searchYear+i}-${searchEndMonth}-${searchEndDay}`;
+        let url =
+        `https://api.github.com/search/repositories?q=language%3A${languageName}+created%3A${startDate}..${endDate}`; 
+        fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => 
+        { 
+            let output = {
+                startDate,
+                endDate,
+                searchYear:searchYear+i,
+                timePeriod:1,
+                repoCount: data.total_count
+            }
         // resolve(`${languageName} ${startDate}..${endDate}:${data.total_count}`);
-        // })
-        // .catch(error =>{ console.log(error); })
-
-        resolve(`${languageName} ${startDate}..${endDate}`);
+        resolve(output);
+        })
+        .catch(error =>{ console.log(error); })
+        // let output = {
+        //     startDate,
+        //     endDate,
+        //     searchYear:searchYear+i,
+        //     timePeriod:1,
+        //     repoCount: 0
+        // }
+        // resolve(`${languageName} ${startDate}..${endDate}`);
+        // resolve(output);
         
     }, 1200 * i)
 
@@ -50,39 +78,65 @@ const timePromise = (languageName,i,searchYear,searchStartMonth,searchStartDay,s
 }
 
 
-const fetchRepoAmount = (languageArray) => {
 
-    //Loop for each language repo amount
-    languageArray.map((language,index) => {
+const fetchSingleLanguage= (lang,selectPeriod) =>{
 
-       
-        
-        let languageName = language.toLowerCase();
-        //Init search query date
-        let searchStartYear = 2015;
-        let searchStartMonth = 1;
-        let searchEndMonth = 6;
-        let searchStartDay = 1;
-        let searchEndDay = 30;
-     
+    let languageName = lang.toLowerCase();
+    //Init search query date
+  
+    let searchStartYear = 2015;
+    let searchStartMonth = '01';
+    let searchEndMonth = '06';
+    let searchStartDay = '01';
+    let searchEndDay = '30';
+    
+    if(selectPeriod!==1){
+        searchStartMonth = '07';
+        searchEndMonth ='12';
+        searchStartDay = '01';
+        searchEndDay = '31';
+    }
+    let outputData = [];
 
-        //Loop for last 5 year
-        for (let i = 0; i < 5; i++) {
+    //Loop for last 5 year
+    for (let i = 0; i < 5; i++) {
 
-            timePromise(languageName,i,searchStartYear,searchStartMonth,searchStartDay,searchEndMonth,searchEndDay)
-            .then(data=>{
-                console.log(data);
+       timePromise(languageName,i,searchStartYear,searchStartMonth,searchStartDay,searchEndMonth,searchEndDay)
+        .then(data=>{
+            console.log('Fetching..'+data.repoCount);
+            outputHTML.innerHTML += `<p>Find ${data.repoCount} in ${data.startDate} to ${data.endDate}</p>`;
+            outputData.push(data);
+            if(i===4)
+            {
                 
-            })
-      
-        }
+                let jsonData = {languageName,
+                data:outputData};
+                console.log(jsonData);
+                outputHTML.innerText = JSON.stringify(jsonData);
+                
+            }
+        })
 
-   
+    }
 
-    });
-
-}
-
-fetchRepoAmount(languages);
+};
 
 
+// fetchSingleLanguage('Python');
+
+fetchBtn.addEventListener('click',(e)=>{
+    
+    let language = selectLanguage.value.toLowerCase().trim();
+    let selectPeriod = document.querySelector('input[name="period"]:checked').value;
+    console.log('Start fetching...'+language);
+    outputHTML.innerHTML = `<p>Start fetching...${language}</p>`;
+    fetchSingleLanguage(language,selectPeriod);
+})
+
+getJSONBtn.addEventListener('click',(e)=>{
+    $.getJSON("./data.json", function(json){
+        console.log(json);
+      });
+});
+
+});
